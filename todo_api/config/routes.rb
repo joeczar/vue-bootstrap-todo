de626e739f2e8300e9e96c25d355dbb6c8b58968
Joe Czarnecki <joe.czar@outlook.com>
2023-11-24 17:26:06 +0100

    feat: created session route & user auth

--- todo_api/config/routes.rb
+++ todo_api/config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :todos
+  resources :sessions, only: [:create]
+  root to: "static#home"
 end
--- todo_api/db/schema.rb
+++ todo_api/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_124744) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_160510) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
